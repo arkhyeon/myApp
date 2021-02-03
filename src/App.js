4914e8ca28d636cb24f81f3d91235674638a234a
qkdwngus <qkdwn48@naver.com>
2021-02-04 00:58:09 +0900

    210204 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 //react 자동적으로 너의 class component의 render method를 실행
 class App extends React.Component{
   state = {
-
-  }
+    count:0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count+ 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count-1}));
+  };
   render(){
-    return <h1>Im a class Component</h1>;
+    return (
+      <div>
+        <h1>number : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
