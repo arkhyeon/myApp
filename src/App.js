9f3319e1c88d46466d4ad9c1243a7c1b20829f42
qkdwngus <qkdwn48@naver.com>
2021-02-07 23:20:56 +0900

    210207

--- src/App.js
+++ src/App.js
@@ -3,35 +3,15 @@ import React from "react";
 
 //react 자동적으로 너의 class component의 render method를 실행
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count:0
+    isLoading : true
   };
-  add = () => {
-    this.setState(current => ({count: current.count+ 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count-1}));
-  };
-  componentDidMount(){
-    console.log("component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I just update");
-  }
-  componentWillUnmount(){
-    console.log("component unmount");
-  }
   render(){
     console.log("I'm render");
     return (
+      const { isLoadging } = this.state;
       <div>
-        <h1>number : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading" : "we are ready"}
       </div>
     );
   }
